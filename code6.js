6a5e09fa5b947b42a4183aa27e380a63cdc2c92e
zharnikov <zharnikov@mbgbeauty.ru>
2018-11-21 18:46:23 +0300

    Add fadeIn/out effects at jsQuery vs js

---
+++ code6.js
@@ -0,0 +1,46 @@
+var $fadeOutButton = $('.fadeOut');
+var $fadeInButton = $('.fadeIn');
+
+var $text = $('.text');
+var $text2 = $('.text2');
+
+var fadeOut = function(element, ms){
+	var opacity = 1;
+	var timer = setInterval(function (){
+		if(opacity <= 0.1){
+			clearInterval(timer);
+			element.style.display = 'none';
+		}
+		element.style.opacity = opacity;
+		opacity -= 0.1;
+	}, ms/10)
+}
+
+var fadeIn = function(element, ms){
+	var opacity = 0.1;
+	element.style.display = 'block';
+	element.style.opacity = opacity;
+	var timer = setInterval(function (){
+		if(opacity >= 1){
+			clearInterval(timer)
+		}
+		element.style.opacity = opacity;
+		opacity += 0.1;
+	}, ms/10)
+}
+
+
+var fadeOutHandler = function (){
+	//$text.fadeOut(3000);
+	fadeOut($text[0], 3000);
+}
+
+var fadeInHandler = function (){
+	//$text2.fadeIn(3000);
+	fadeIn($text2[0], 3000);
+}
+
+$fadeOutButton.click(fadeOutHandler);
+$fadeInButton.click(fadeInHandler);
+
+	
