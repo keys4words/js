0a84f078e717de9420dbc346208f4adccb64565c
Max 19038 <keys4words@gmail.com>
2018-12-06 23:03:15 +0300

    Update programmer2017

--- programmer2017-les5.js
+++ programmer2017-les5.js
@@ -28,22 +28,35 @@ function mathOp(a, b, op){
 	return res;
 }
 
-// var x, y, res, answer;
-// for(var i = 0; i < 10; i++){
-// 	x = randomInt(3, 15);
-// 	y = randomInt(3, 15);
-// 	res = x * y;
-// 	answer = +prompt(x + ' * ' + y + ' = ?');
-// 	if(res == answer){
-// 		console.log('Nice guy');
-// 	}else{
-// 		console.log('Stupid');
-// 	}
+function codeToOp(num){
+	
+	return variants[num];
+}
 
-// }
+var x, y, res, op, answer, answerStr;
+var variants = ['+', '-', '*', '/'];
 
-console.log(mathOp(1, 5, '+'));
-console.log(mathOp(1, 5, '-'));
-console.log(mathOp(1, 5, '*'));
-console.log(mathOp(1, 5, '/'));
+for(var i = 0; i < 10; i++){
+	x = randomInt(3, 15);
+	y = randomInt(3, 15);
+	op = variants[randomInt(0, 3)];
+	res = mathOp(x, y, op);
+	
+	
+	do{
+		answerStr = prompt(x + ' ' + op + ' ' + y + ' = ?'); 
+		answer = +answerStr;
+	}while(answerStr == '' || isNaN(answer));
+
+	if(res.toFixed(2) == answer.toFixed(2)){
+		console.log('Nice guy');
+	}else{
+		console.log('Stupid ' + x + ' ' + op + ' ' + y + ' = ' + res.toFixed(2));
+	}
+}
+
+// console.log(mathOp(1, 5, '+'));
+// console.log(mathOp(1, 5, '-'));
+// console.log(mathOp(1, 5, '*'));
+// console.log(mathOp(1, 5, '/'));
 </script>
