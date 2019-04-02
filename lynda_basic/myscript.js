b8941befa729e342bedec94f14b459cdc369e982
zharnikov <zharnikov@mbgbeauty.ru>
2019-04-02 18:50:49 +0300

    add lynda basics

---
+++ lynda_basic/myscript.js
@@ -0,0 +1,13 @@
+var year = 2019;
+var x = '2019';
+var month = 4, day = 2;
+var month = 'April';
+var currentMonth = true;
+
+var message;
+if(year === x){
+    message = "true";
+} else {
+    message = "false";
+}
+alert(message);
