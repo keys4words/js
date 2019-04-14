473cd4fba80cb901adfab51b541585dbb73df5ab
Max 19038 <keys4words@gmail.com>
2019-04-14 19:10:49 +0300

    add booleans

---
+++ jsBootcamp/basics/booleans.js
@@ -0,0 +1,8 @@
+let temp = 'Max'
+let isFreezing = temp === 'Max'
+
+console.log(isFreezing)
+
+let age = 25
+let isChild = age <= 7
+let isSenior = age >= 65
--- jsBootcamp/basics/hello.js
+++ jsBootcamp/basics/hello.js
@@ -1,3 +1,11 @@
-let num = 43.9 + 2
+let age = 26
+let dogYears = (age + 1)/7
+//simple calculation
+console.log(dogYears)
 
-console.log(num)
+let petName = 'Hal'
+petName = 'Spot'
+console.log(petName)
+
+let res = 8*6
+console.log(res)
