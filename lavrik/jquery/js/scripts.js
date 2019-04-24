d6bf9aa9a25a2b8f7ceb5e9eb3590dbce32807c5
keys4words <keys4words@gmail.com>
2019-04-24 14:46:28 +0300

    start jquery from Lavrik

---
+++ lavrik/jquery/js/scripts.js
@@ -0,0 +1,31 @@
+$(function(){
+	var elem = $('')
+});
+/* $(document).ready(function(){
+	var elem = $('#menu li');
+	
+	// html() -> return innerHTML                 get
+	// html('123') -> innerHTML = '123'			  set
+	
+	// var res = elem.html(); // innerHTML
+	// alert(res);
+	
+	// css('color') -> return style.color 		  get
+	// css('color', 'red') -> style.color = red   set
+	// css({a: b, c: d, e: f}) -> style.a=b style.c=d style.e=f 
+	
+	// elem.css('background', 'red'); // style
+	 
+	// var res = elem.css('background-color'); // получаем значение только для 1-ого элемента !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// alert(res);
+	
+	/* elem.css('background', 'red');
+	elem.css('padding', '5px');
+	elem.css('border', '10px solid #392');
+	
+	var styles = {background: 'red', padding: '5px', border: '10px solid #392',
+					'border-radius': '20px'};
+	elem.css(styles); */
+	
+/*	elem.addClass('bred_color');
+}); */
