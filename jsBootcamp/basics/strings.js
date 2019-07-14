f5e8ab047a9c5386c172f9dd68b2732b356a9374
Max 19038 <keys4words@gmail.com>
2019-07-14 22:36:44 +0300

    repeat strings intro

---
+++ jsBootcamp/basics/strings.js
@@ -0,0 +1,11 @@
+let firstName = "Joe"
+let secondName = 'Dow'
+let fullName = firstName + ' ' + secondName
+
+console.log(fullName)
+
+let city = "Moscow"
+let country = 'Russia'
+let location = city + ', ' + country
+
+console.log(location)
