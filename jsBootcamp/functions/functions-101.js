c35cbe510bd2092f1db7d63a866205deadec4b34
keys4words <keys4words@gmail.com>
2019-07-19 17:08:55 +0300

    Add functions intro

---
+++ jsBootcamp/functions/functions-101.js
@@ -0,0 +1,18 @@
+
+let greetUser = function(){
+    console.log('Welcome user')
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+let square = function(x){
+    let result = x * x
+    return result
+}
+
+let value = square(4)
+let otherValue = square(10)
+console.log(value)
+console.log(otherValue)
