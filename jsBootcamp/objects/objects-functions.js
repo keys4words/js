30b203d2134c927d1ba750ac0f8a01b6750948d3
keys4words <keys4words@gmail.com>
2019-07-29 14:03:40 +0300

    add objects module

---
+++ jsBootcamp/objects/objects-functions.js
@@ -0,0 +1,33 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+let otherBook = {
+    title: 'A Peoples History',
+    author: 'Howard Zinn',
+    pageCount: 723
+}
+
+let getSummary = function(book){
+    return {
+        summary: `${book.title} by ${book.author}`,
+        pageCountSummary: `${book.title} is ${book.pageCount} pages long` 
+    }
+}
+
+let bookSummary = getSummary(myBook)
+console.log(bookSummary)
+console.log(getSummary(otherBook))
+
+let convertFahrenheit = function(fahrenheit){
+    return {
+        fahrenheit: fahrenheit,
+        kelvin: (fahrenheit + 459.67) * (5/9),
+        celsius: (fahrenheit - 32) * (5/9)
+    }
+}
+
+let temps = convertFahrenheit(74)
+console.log(temps)
