adbfc11207b29c4cab70cc32fcd526689a1c909a
keys4words <keys4words@gmail.com>
2020-04-16 21:20:58 +0300

    add mobile bonus

---
+++ js-dev/mobile.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', function(){
+    let box = document.querySelector('.box');
+
+    // box.addEventListener('touchstart', function(e){
+    //     e.preventDefault();
+    //     console.log("Red box: touchstart");
+    //     console.log(e.target);
+    //     console.log(e.touches[0].target);
+    //     console.log(e.changedTouches);
+    //     console.log(e.targetTouches);
+        
+    // });
+
+    box.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+        console.log("Red box: " + e.touches[0].pageX);
+    });
+
+    // box.addEventListener('touchend', function (e) {
+    //     e.preventDefault();
+    //     console.log("Red box: touchend");
+    // });
+
+    // new RegExp('pattern', 'flags');
+    let ans = prompt('Input your name');
+    let reg = /n/g;
+    // console.log(ans.search(reg));
+    console.log(ans.match(reg));
+    
+
+
+});
