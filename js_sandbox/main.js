6daa354f003d0619e5e024723ed2702c7aeb91fb
keys4words <keys4words@gmail.com>
2020-09-03 14:59:41 +0300

    add js sandbox folder

---
+++ js_sandbox/main.js
@@ -0,0 +1,27 @@
+console.log('Hello World');
+console.error('This is error');
+console.warn('This is warning');
+
+//var - deprecated, let, const
+let age = 30;
+age = 34;
+// console.log(age);
+const male = 'man';
+// male = 'female';
+// console.log(male);
+
+// Strings, Numbers, Boolean, null, underfined
+const name = 'John Smith';
+const age1 = 34;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+// console.log(typeof z);
+
+// strings operations
+console.log(name + ' something');
+console.log(`My name is ${name}`);
+console.log(name.length);
