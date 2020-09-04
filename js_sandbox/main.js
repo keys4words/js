3079a30ddf64bf33d091e48c88f1ef7bfbd7b8fc
keys4words <keys4words@gmail.com>
2020-09-04 16:37:15 +0300

    add arrays and intro to objects

--- js_sandbox/main.js
+++ js_sandbox/main.js
@@ -25,3 +25,39 @@ let z;
 console.log(name + ' something');
 console.log(`My name is ${name}`);
 console.log(name.length);
+let s = 'technology, computers, it, code';
+console.log(name.substring(0, 4).toUpperCase());
+console.log(s.split(', '));
+
+// arrays
+const numbers = new Array(1, 2, 'string', false, 5);
+console.log(numbers);
+console.log(numbers[2]);
+numbers[3] = 'new string';
+console.log(numbers);
+numbers.push('orange');
+console.log(numbers);
+numbers.unshift('orange');
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+console.log(Array.isArray('hello'));
+console.log(numbers.indexOf('string'));
+
+
+// objects
+const person = {
+    firstName: 'James',
+    lastName: 'Bond',
+    age: 45,
+    hobbies: ['shooting', 'sex', 'drinking'],
+    address: {
+        state: 'GB',
+        city: 'London',
+        street: 'Bakes str'
+    }
+};
+console.log(person.address.city);
+const {firstName, lastName} = person;
+console.log(lastName);
+
