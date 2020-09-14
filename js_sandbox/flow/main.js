b93cbc9b96a9540e8e573891b57eef06b4bf3bdd
keys4words <keys4words@gmail.com>
2020-09-14 10:21:15 +0300

    start js flow in js_sandbox

---
+++ js_sandbox/flow/main.js
@@ -0,0 +1,9 @@
+var number1 = '345';
+var number2 = [1,51,6, 45];
+number2.push(999);
+// number2.sort();
+number2.reverse();
+number2.forEach(function(el){
+    console.log(el);
+})
+console.log(number2);
