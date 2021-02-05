703121324bf88107e44fcaefbc73d7c9f04ac528
keys4words <keys4words@gmail.com>
2021-02-05 09:18:50 +0300

    add binder

--- vue/script.js
+++ vue/script.js
@@ -1,8 +1,22 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Counter'
+            placeholderString: 'Input name of note',
+            title: 'List Notes',
+            inputValue: '',
+            notes: ['Note One', 'Note Two', 'Note #3']
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNewNote(){
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+        },
+        removeNote(index){
+            this.notes.splice(index, 1)
         }
     }
 }
