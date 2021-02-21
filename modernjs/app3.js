55a3a58d2aca0158c4125a9b31438f014bae8e6a
keys4words <keys4words@gmail.com>
2021-02-21 18:53:43 +0300

    add async in modernjs

--- modernjs/app3.js
+++ modernjs/app3.js
@@ -18,11 +18,39 @@ const person = {
 //console.log(person)
 
 
-const {name, isBoxer, greet} = person
-console.log(name, isBoxer, greet())
+// const {name, isBoxer, greet} = person
+// console.log(name, isBoxer, greet())
 
-for (let key in person) {
-    if (person.hasOwnProperty(key)) {
-        console.log(key + ' -> ' + person[key])
+// for (let key in person) {
+//     if (person.hasOwnProperty(key)) {
+//         console.log(key + ' -> ' + person[key])
+//     }
+// }
+
+const logger = {
+    keys() {
+        console.log('Object keys: ', Object.keys(this))
+    },
+    keysAndValues(){
+        Object.keys(this).forEach(function(key){
+            console.log(`${key} -> ${this[key]}`)
+        }).bind(this)
+    },
+    withParams(top=false, between=false, bottom=false){
+        if(top){
+            console.log('--------start---------')
+        }
+        Object.keys(this).forEach((key, index, array) => {
+            console.log(`${key} -> ${this[key]}`)
+            if(between && index !== array.length-1) {
+                console.log('--------------------')
+            }
+        })
+        if(bottom){
+            console.log('--------end---------')
+        }
     }
 }
+
+// logger.withParams.call(person, true, true, true)
+logger.withParams.apply(person, [true, true, true])
