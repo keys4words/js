c4d5a009385eadd70b24ee19af625ea280851eb7
keys4words <keys4words@gmail.com>
2021-02-22 21:30:24 +0300

    finished forms

--- modernjs/forms/app.js
+++ modernjs/forms/app.js
@@ -10,15 +10,23 @@
 //     document.querySelector('span').innerHTML = document.querySelector('#psw').value
 // }
 
-document.querySelector('#btn-1').onclick = () => {
-        // console.log(document.querySelector('#chk').value)
-        let checkBoxValue = document.querySelector('#chk')
-        // console.log(checkBoxValue.checked);
-        if (checkBoxValue.checked){
-            console.log('element pressed');
-        }
-        else {
-            console.log('element NOT pressed');
-        }
-    }
-    
+// document.querySelector('#btn-1').onclick = () => {
+//         // console.log(document.querySelector('#chk').value)
+//         let checkBoxValue = document.querySelector('#chk')
+//         // console.log(checkBoxValue.checked);
+//         if (checkBoxValue.checked){
+//             console.log('element pressed');
+//         }
+//         else {
+//             console.log('element NOT pressed');
+//         }
+//     }
+document.querySelector('#btn-2').onclick = (e) => {
+    e.preventDefault()
+    // let text = document.querySelector('#text')
+    // console.log(text.value)
+    // text.value = 'Default'
+    let form = document.querySelector('form')
+    console.log(form.elements.input.value)
+    console.log(form.elements.text.value)
+}
