df524ceed2d2be73dfc48b4670ff2571da0e01d9
keys4words <keys4words@gmail.com>
2021-03-01 13:00:54 +0300

    add async

---
+++ questions/async.js
@@ -0,0 +1,15 @@
+console.log('Start')
+
+function timout2sec(){
+    console.log('timout2sec')
+}
+
+setTimeout(function(){
+	console.log('inside timeout after 4 sec')
+}, 0)
+
+setTimeout(timout2sec, 2000)
+
+console.log('end')
+
+// http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
