e5c087be29d1b8abdef170176d224c6e61def405
keys4words <keys4words@gmail.com>
2021-03-28 19:45:04 +0300

    start asyncAwait

--- questions/asyncAwait.js
+++ questions/asyncAwait.js
@@ -2,4 +2,20 @@ const delay = ms => {
     return new Promise(r => setTimeout(() => r(), ms))
 }
 
-delay(2000).then(() => console.log('Something'))
+// delay(2000).then(() => console.log('Something'))
+
+const url = 'https://jsonplaceholder.typicode.com/todos' 
+function fetchTodos(){
+    console.log('Fetch todo started...')
+    return delay(2000)
+        .then(()=> {
+             return fetch(url)
+    }).then(response => response.json())
+}
+
+
+fetchTodos()
+    .then(data => {
+        console.log('Data:', data)
+    })
+    .catch(e => console.error(e))
